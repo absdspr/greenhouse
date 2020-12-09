e1cce359f71254c9695cb52c99c83aa9792088de
lighrik <kiriys36@gmail.com>
2020-12-09 23:40:40 +0300

    refactor(sensors data) removed post route

--- src/routes/sensorsData.js
+++ src/routes/sensorsData.js
@@ -8,7 +8,4 @@ const controller = require('../controllers/sensorsData')
 router.get('/', controller.getSensorsData)
 
 
-// POST /sensorsData
-// временное решение для теста апишки, в будущем будет убранно
-router.post('/', controller.postSensorsData)
 module.exports = router
